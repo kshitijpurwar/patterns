657d22564caf12efc2d22f4d6db3de84dc9d01f6
Kshitij Purwar <kshitij.purwar4@gmail.com>
2017-11-25 00:12:54 +0530

    Initial commit

---
+++ cli.js
@@ -0,0 +1,14 @@
+var obj = {};
+obj
+var objectCreated = Object.create(Object.prototype)
+var objectNew = new Object();
+obj
+objectNew
+objectCreated
+// this a comment
+'lol' || []
+'lol' && []
+1 || 0 || []
+1 && 0 && []
+null || 0 || [] || false || 1
+null || 0 || [] || false
