85bba58c94241db435d7b1c83fd85a880d1a7c90
Kshitij Purwar <kshitij.purwar4@gmail.com>
2017-11-25 01:58:55 +0530

    Revealing module pattern,list of methods in return and definition elsewhere

--- taskRepo.js
+++ taskRepo.js
@@ -6,18 +6,23 @@
 
 // This module encapsulates all the methods
 var repo = function(){
-  return {
-    get: function(id) {
-      console.log('Getting task from local storage');
-      return {
-        name : 'A new task from storage'
-      }
-    },
-
-    save: function(id) {
-      console.log('Saving task in the local storage');
+  
+  var get = function(id) {
+    console.log('Getting task from local storage');
+    return {
+      name : 'A new task from storage'
     }
   }
+
+  var save = function(id) {
+    console.log('Saving task in the local storage');
+  }
+
+  return {
+    get: get,
+    save: save
+  }
+
 }
 
 module.exports = repo();
